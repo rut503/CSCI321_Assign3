20674a732a12a5d76ebb8a7226b159a8eca77b16
rut503 <rutvik503@gmail.com>
2020-10-07 12:00:38 -0500

    Initial Commit

---
+++ CSCI321_Assign3/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  CSCI321_Assign3
+//
+//  Created by Rutvik Patel on 10/7/20.
+//  Copyright © 2020 Rut Codes. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CSCI321_Assign3/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CSCI321_Assign3
+//
+//  Created by Rutvik Patel on 10/7/20.
+//  Copyright © 2020 Rut Codes. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
