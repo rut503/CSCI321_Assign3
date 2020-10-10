cca7870d454c6888e4fb45e98b76ac184fde197b
hardik patel <hp9423605066@gmail.com>
2020-10-10 16:15:57 -0500

    yet to do extra credit part 2 and documentation

--- CSCI321_Assign3/StopwatchViewController.swift
+++ CSCI321_Assign3/StopwatchViewController.swift
@@ -14,8 +14,9 @@ class StopwatchViewController: UIViewController {
     
     @IBOutlet weak var stopwatchTimePicker: UIDatePicker!
     
+    @IBOutlet weak var playButtonOutlet: UIBarButtonItem!
     @IBOutlet weak var timeLabel: UILabel!
-    
+    @IBOutlet weak var stopButtonOutlet: UIBarButtonItem!
     var timeLeft = 60
     var timer = Timer()
     
@@ -30,29 +31,44 @@ class StopwatchViewController: UIViewController {
         timeLeft = Int(stopwatchTimePicker.countDownDuration)
         
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(decrementTimer), userInfo: nil, repeats: true)
-        
-        sender.isEnabled = false
+    
+        playButtonOutlet.isEnabled = false
+        stopButtonOutlet.isEnabled = true
     }
     
     @IBAction func stopButton(_ sender: UIBarButtonItem) {
          timer.invalidate()
+        timeFormatter(time: Int(stopwatchTimePicker.countDownDuration))
+        stopButtonOutlet.isEnabled = false
+        playButtonOutlet.isEnabled = true
+        
+    }
+    
+    @IBAction func timePickerAction(_ sender: UIDatePicker) {
+        if (!timer.isValid) {
+            timeFormatter(time: Int(stopwatchTimePicker.countDownDuration))
+        }
     }
     
     @objc func decrementTimer(){
         
-        var minutes: Int
-        var seconds: Int
         timeLeft -= 1
-        
-        minutes = Int (timeLeft) / 60 % 60
-        seconds = Int(timeLeft) % 60
-        
-        timeLabel.text = String(format: "%02d:%02d",minutes,seconds)
+        timeFormatter(time: timeLeft)
         
         if timeLeft <= 0 {
             timer.invalidate()
         }
         
+    }
+    
+    func timeFormatter (time: Int) {
+        var minutes: Int
+        var seconds: Int
+        
+        minutes = Int (time) / 60 % 60
+        seconds = Int(time) % 60
+        
+        timeLabel.text = String(format: "%02d:%02d",minutes,seconds)
     }
     
 }
