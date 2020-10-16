a4aa043b820bfa655b3e05ab98819d53f3537f7f
rut503 <rutvik503@gmail.com>
2020-10-15 21:35:20 -0500

    there were still bugs in timer,which are now fixed, there is one more odd feature but don't have time to fix that right now, documentation for all files checked and completed.

--- CSCI321_Assign3/StopwatchViewController.swift
+++ CSCI321_Assign3/StopwatchViewController.swift
@@ -17,14 +17,13 @@ class StopwatchViewController: UIViewController {
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var stopButtonOutlet: UIBarButtonItem!
     @IBOutlet weak var pauseButtonOutlet: UIBarButtonItem!
-    var timeLeft = 60                       //initial value 1 min
+    
+    var timeLeft = 60       //initial value 1 min
     var timer = Timer()
-    var flag = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        timeFormatter(time: timeLeft)                  //print out initial time
         pauseButtonOutlet.isEnabled = false            //disabling pause button
         stopButtonOutlet.isEnabled = false             //disabling stop button
     }
@@ -33,13 +32,7 @@ class StopwatchViewController: UIViewController {
      Play button which starts the clock and decrement the time by calling "decreamentTimer". It also enables pause and stop buttons.
      */
     @IBAction func startButton(_ sender: UIBarButtonItem) {
-        
-        if ( !flag ) {   //only reset the clock the first time
-            timeLeft = Int(stopwatchTimePicker.countDownDuration)
-        }
-        flag = true
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(decrementTimer), userInfo: nil, repeats: true)
-        
         playButtonOutlet.isEnabled = false       //disables play button
         pauseButtonOutlet.isEnabled = true       //enables pause button
         stopButtonOutlet.isEnabled = true        //enables stop button
@@ -49,29 +42,32 @@ class StopwatchViewController: UIViewController {
      Pause button to pause the timer. It also enables the play button once we pause the timer.
      */
     @IBAction func pauseButton(_ sender: UIBarButtonItem) {
-        timer.invalidate()                       //pause timer
+        timer.invalidate()                      //pause timer
         pauseButtonOutlet.isEnabled = false     //disables pause button
         playButtonOutlet.isEnabled = true       // enables play button
-        flag = true
+        
     }
     
     /**
      Stop button to Reset the timer when clicked. This button is enabled when clock is running.
      */
     @IBAction func stopButton(_ sender: UIBarButtonItem) {
-        timer.invalidate()
+        timer.invalidate()  // stop the timer
         timeLeft = Int(stopwatchTimePicker.countDownDuration)    // reset the timer to value selected
         timeFormatter(time: Int(stopwatchTimePicker.countDownDuration))
         stopButtonOutlet.isEnabled = false      //disables stop button
+        pauseButtonOutlet.isEnabled = false     //disables pause button
         playButtonOutlet.isEnabled = true       //enalbes play button
     }
     
     /**
-     Updates the time clock as we select the time in stop watch
+     Updates the time clock as we select the time from UI Time Picker
      */
     @IBAction func timePickerAction(_ sender: UIDatePicker) {
-        if (!timer.isValid && !flag) {
-            timeFormatter(time: Int(stopwatchTimePicker.countDownDuration))
+        // if the timer is stopped then allow time picker to count down time
+        if (!timer.isValid) {
+            timeLeft = Int(stopwatchTimePicker.countDownDuration) // resetting timer
+            timeFormatter(time: Int(sender.countDownDuration))    // printing new time
         }
     }
     
@@ -79,27 +75,28 @@ class StopwatchViewController: UIViewController {
      Calculates time left, by decrement it by one second each and simultaneously printing it out timeFormatter()
      */
     @objc func decrementTimer(){
+        timeLeft -= 1                   // descrease time by 1 second
+        timeFormatter(time: timeLeft)   // print the time in time format
         
-        timeLeft -= 1
-        timeFormatter(time: timeLeft)
-        
+        // if the count down hits 0, stop the timer.
         if timeLeft <= 0 {
             timer.invalidate()
         }
-        
     }
     
     /**
-     Prints out the stopwatch time in right format i.e., in minutes and seconds ( MM:SS )
+     Prints out the stopwatch time in right format i.e., in hours, minutes and seconds ( HH:MM:SS )
      */
     func timeFormatter (time: Int) {
+        var hours: Int
         var minutes: Int
         var seconds: Int
         
-        minutes = Int (time) / 60 % 60
+        hours = Int(time) / 60 / 60 % 60
+        minutes = Int(time) / 60 % 60
         seconds = Int(time) % 60
         
-        timeLabel.text = String(format: "%02d:%02d",minutes,seconds)
+        timeLabel.text = String(format: "%02d:%02d:%02d",hours,minutes,seconds)
     }
     
 }
--- CSCI321_Assign3/TemperatureConversionViewController.swift
+++ CSCI321_Assign3/TemperatureConversionViewController.swift
@@ -13,47 +13,55 @@ import UIKit
 class TemperatureConversionViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
     @IBOutlet weak var unitsOfConversion: UISegmentedControl!
-    
     @IBOutlet weak var tempAmountPicker: UIPickerView!
-    
     @IBOutlet weak var convertedValueLabel: UILabel!
     
-    var unitToConvertFrom = "F"
-    
-    let fahrenheitValues: [Int] = Array(-129...134)
-    let celsiusValues: [Int] = Array(-90...57)
+    var unitToConvertFrom = "F"        // flag to keep tract of which conversion is needed
+    let fahrenheitValues: [Int] = Array(-129...134) // possible fahrenheit temp values to convert from
+    let celsiusValues: [Int] = Array(-90...57) // possible celsius temp values to convert from
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // initial conversion to display when app loads.
         convertFahrenheitToCelsius(fahrenheits: fahrenheitValues[tempAmountPicker.selectedRow(inComponent: 0)])
     }
     
-    
+    /**
+     This function sets converison flag which indicates which type of conversion is needed according to the Segment choosen by the user and then does
+     the initial conversion based on the first row value in the temp data picker.
+     */
     @IBAction func unitsOfConversionPicked(_ sender: UISegmentedControl) {
+        // selectes first row from the data picker to avoid index out of bounds.
         tempAmountPicker.selectRow(0, inComponent: 0, animated: true)
         
+        // if the user chose to convert F to C
         if(sender.selectedSegmentIndex == 0){
-            unitToConvertFrom = "F"
+            unitToConvertFrom = "F"   // set the flag
+            // initial conversion based on the first row of the data picker.
             convertFahrenheitToCelsius(fahrenheits: fahrenheitValues[tempAmountPicker.selectedRow(inComponent: 0)])
         }
+        // if the user chose to convert C to F
         else {
-            unitToConvertFrom = "C"
+            unitToConvertFrom = "C"   // set the flag
+            // initial conversion based on the first row of the data picker.
             convertCelsiusToFahrenheit(celsius: celsiusValues[tempAmountPicker.selectedRow(inComponent: 0)])
         }
         
-        tempAmountPicker.reloadAllComponents()
+        tempAmountPicker.reloadAllComponents() // reload the component so that new temp data shows in the data picker
     }
     
     // MARK: Picker View Data Source methods
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
+        return 1 // using one component for data picker
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        // if the Picker asks for number of rows in Fahrenheit data array
         if(unitToConvertFrom == "F"){
             return fahrenheitValues.count
         }
+        // if the Picker asks for number of rows in Celsius data array
         else {
             return celsiusValues.count
         }
@@ -61,30 +69,49 @@ class TemperatureConversionViewController: UIViewController, UIPickerViewDelegat
 
     // MARK: Picker View Delegate method
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        // returning data for Fahrenheit data picker
         if(unitToConvertFrom == "F"){
             return String(fahrenheitValues[row])
         }
+        // returning data for Celsius data picker
         else{
             return String(celsiusValues[row])
         }
     }
     
+    /**
+     This function runs when user selects a row in the data picker and converts the selected temparate from desired unit to another unit.
+     */
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // if the temp picked is Fahrenheit
         if(unitToConvertFrom == "F"){
+            // convert Fahrenheit amount to Celsius amount.
             convertFahrenheitToCelsius(fahrenheits: fahrenheitValues[row])
         }
+        // if the temp picked is Celsius
         else{
+            // convert Celsius amount to Fahrenheit amount.
             convertCelsiusToFahrenheit(celsius: celsiusValues[row])
         }
     }
     
+    /**
+     This function converts Fahrenheit Temp to Celsius Temp and displays result on a label.
+     */
     func convertFahrenheitToCelsius(fahrenheits: Int) {
+        // F to C conversion formula
         let temp = Int((Double(fahrenheits) - 32.0) * (5.0 / 9.0))
+        // display the result
         convertedValueLabel.text = String(temp) + "°C"
     }
     
+    /**
+     This function converts Celsius Temp to Fahrenheit Temp and displays result on a label.
+    */
     func convertCelsiusToFahrenheit(celsius: Int) {
+        // C to F conversion formula
         let temp = Int((Double(celsius) * 9.0 / 5.0) + 32)
+        // display the result
         convertedValueLabel.text = String(temp) + "°F"
     }
 }
